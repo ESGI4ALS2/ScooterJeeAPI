1e3b1072e0815a73daa8187374b2d59554f0e374
ame <amedouillard@hotmail.fr>
2022-07-17 22:55:21 +0200

    test ci/cd

--- src/main/java/com/scooterjee/app/expostion/user/controller/UserController.java
+++ src/main/java/com/scooterjee/app/expostion/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.scooterjee.app.expostion.user.controller;
 
 import com.scooterjee.app.domain.address.Address;
+import com.scooterjee.app.domain.address.exception.InvalidAddressException;
 import com.scooterjee.app.domain.user.User;
 import com.scooterjee.app.expostion.category.dto.CategoryDTO;
 import com.scooterjee.app.expostion.error.ErrorHandler;
@@ -37,7 +38,11 @@ public class UserController extends ErrorHandler {
     private String token;
 
 
-    public UserController(UserService userService, AddressService addressService, ProblemService problemService) {
+    public UserController(
+        UserService userService,
+        AddressService addressService,
+        ProblemService problemService
+    ) {
         this.userService = userService;
         this.addressService = addressService;
         this.problemService = problemService;
@@ -45,15 +50,23 @@ public class UserController extends ErrorHandler {
 
     @GetMapping(value = "/users")
     public List<UserDTO> getUsers() {
-        return userService.getAll().stream()
-                .map(user -> new UserDTO(
-                        user.getEmailAddress().toString(),
-                        user.getLastName(),
-                        user.getFirstName(),
-                        user.getAddress().toString(),
-                        user.getPhoneNumber(),
-                        user.getAssignedRoles().stream().map(role -> new RoleDTO(role.getID(), role.getName())).collect(Collectors.toList())))
-                .collect(Collectors.toList());
+
+        return userService
+            .getAll()
+            .stream()
+            .map(user -> new UserDTO(
+                user.getEmailAddress().toString(),
+                user.getLastName(),
+                user.getFirstName(),
+                user.getAddress().toString(),
+                user.getPhoneNumber(),
+                user.getAssignedRoles()
+                    .stream()
+                    .map(role -> new RoleDTO(role.getID(), role.getName()))
+                    .collect(Collectors.toList())
+            ))
+            .collect(Collectors.toList()
+        );
     }
 
     @GetMapping(value = "/users/{email}")
@@ -65,13 +78,23 @@ public class UserController extends ErrorHandler {
     @GetMapping(value = "/users/{email}/categories")
     public List<CategoryDTO> getUserCategories(@PathVariable @Valid String email) {
         User user = userService.getByEmail(new EmailAddress(email));
-        return user.getAssignedCategories().stream().map(categories -> new CategoryDTO(categories.getID(), categories.getName())).collect(Collectors.toList());
+        return user
+            .getAssignedCategories()
+            .stream()
+            .map(categories -> new CategoryDTO(categories.getID(), categories.getName()))
+            .collect(Collectors.toList()
+        );
     }
 
     @GetMapping(value = "/users/{email}/roles")
     public List<RoleDTO> getUserRoles(@PathVariable @Valid String email) {
         User user = userService.getByEmail(new EmailAddress(email));
-        return user.getAssignedRoles().stream().map(role -> new RoleDTO(role.getID(), role.getName())).collect(Collectors.toList());
+        return user
+            .getAssignedRoles()
+            .stream()
+            .map(role -> new RoleDTO(role.getID(), role.getName()))
+            .collect(Collectors.toList()
+        );
     }
 
     @GetMapping(value = "/users/{email}/availableproblems")
@@ -88,49 +111,74 @@ public class UserController extends ErrorHandler {
     }
 
     @PostMapping(value = "/users/{email}/categories")
-    public void addUserCategories(@PathVariable @Valid String email, @RequestBody @Valid UserCategoriesDTO userCategoriesDTO) {
+    public void addUserCategories(
+        @PathVariable @Valid String email,
+        @RequestBody @Valid UserCategoriesDTO userCategoriesDTO
+    ) {
         for (Long id: userCategoriesDTO.list ) {
             userService.addCategoryToUser(new EmailAddress(email), id);
         }
     }
 
     @PostMapping(value = "/users/{email}/roles")
-    public void addUserRoles(@PathVariable @Valid String email, @RequestBody @Valid UserCategoriesDTO userCategoriesDTO) {
+    public void addUserRoles(
+        @PathVariable @Valid String email,
+        @RequestBody @Valid UserCategoriesDTO userCategoriesDTO
+    ) {
         for (Long id: userCategoriesDTO.list ) {
             userService.addRoleToUser(new EmailAddress(email), id);
         }
     }
 
     @PostMapping(value = "/users/{email}/roles/{id}")
-    public void addUserRoles(@PathVariable @Valid String email, @PathVariable @Valid Long id) {
+    public void addUserRoles(
+        @PathVariable @Valid String email,
+        @PathVariable @Valid Long id
+    ) {
         userService.addRoleToUser(new EmailAddress(email), id);
     }
 
     @PostMapping(value = "/users")
     public void addUser(@RequestBody @Valid CreateUserDTO createUserDTO) {
         JOpenCageGeocoder jOpenCageGeocoder = new JOpenCageGeocoder(token);
-        JOpenCageForwardRequest request = new JOpenCageForwardRequest(
-                createUserDTO.address.number + " " +
-                        createUserDTO.address.street + ", " +
-                        createUserDTO.address.postalCode);
+
+        String addressAsString = String.format(
+            "%s %s, %s",
+            createUserDTO.address.number,
+            createUserDTO.address.street,
+            createUserDTO.address.postalCode
+        );
+
+        JOpenCageForwardRequest request = new JOpenCageForwardRequest(addressAsString);
+
         request.setRestrictToCountryCode("fr");
 
         JOpenCageResponse response = jOpenCageGeocoder.forward(request);
         JOpenCageLatLng firstResultLatLng = response.getFirstPosition();
 
+        if(null == firstResultLatLng){
+            System.out.println("Not working. Setting default values");
+            firstResultLatLng = new JOpenCageLatLng();
+            firstResultLatLng.setLat(0.0);
+            firstResultLatLng.setLng(0.1);
+        }
+
         Address address = new Address(
-                null,
-                createUserDTO.address.city,
-                createUserDTO.address.street,
-                createUserDTO.address.number,
-                createUserDTO.address.country,
-                createUserDTO.address.postalCode,
-                firstResultLatLng.getLat(),
-                firstResultLatLng.getLng());
+            null,
+            createUserDTO.address.city,
+            createUserDTO.address.street,
+            createUserDTO.address.number,
+            createUserDTO.address.country,
+            createUserDTO.address.postalCode,
+            firstResultLatLng.getLat(),
+            firstResultLatLng.getLng()
+        );
 
         addressService.add(address);
 
-        userService.add(new User(
+        //TODO vérifier qu'un utilisateur avec cette adresse mail n'existe pas déjà : sinon il n'est pas enregistré en base
+        userService.add(
+            new User(
                 null,
                 createUserDTO.firstname,
                 createUserDTO.lastname,
@@ -138,11 +186,15 @@ public class UserController extends ErrorHandler {
                 createUserDTO.phoneNumber,
                 new EmailAddress(createUserDTO.email),
                 address
-        ));
+            )
+        );
     }
 
     @DeleteMapping(value = "/users/{email}/roles/{id}")
-    public void deleteRoleForUser(@PathVariable @Valid String email, @PathVariable @Valid Long id) {
+    public void deleteRoleForUser(
+        @PathVariable @Valid String email,
+        @PathVariable @Valid Long id
+    ) {
         userService.removeRoleToUser(new EmailAddress(email), id);
     }
 }
